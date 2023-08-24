d711a6f8cc5f701ac29dc3c508ce7f61804c60b7
Niya Mateeva (Nalan Bilgin) <119063181+Polishko@users.noreply.github.com>
2023-08-24 22:41:20 +0300

    Added song scraper
    Song scraper to use with genius library

---
+++ graph_composer/third_version/my_lyrics.py
@@ -0,0 +1,39 @@
+# get an api_key from https://genius.com/api-clients and save it securely in a file
+
+import api_key # the file that contains your api key
+import lyricsgenius
+import os
+
+client_access_token = api_key.my_client_access_token
+
+artists = ["Diana Ross", "Stevie Wonder", "Tom Browne", "Genesis"]  # add your own list of artists
+parent_dir = "some_dir_path" # the main directory that will contain artist name folders with their song files
+genius = lyricsgenius.Genius(client_access_token)
+
+
+def add_lyrics(artists_list, max_song_count):  # Write lyrics of given count of songs by each artist in the list
+    # create directory for each artist
+    for name in artists_list:
+        directory = name
+        path = os.path.join(parent_dir, directory)
+
+        try:
+            os.mkdir(path)
+        except OSError:
+            continue
+    # add songs for each artist
+    for name in artists_list:
+        path = f"{parent_dir}/{name}"
+        try:
+            songs = (genius.search_artist(name, max_songs=max_song_count, sort="popularity")).songs
+
+            for song in songs:
+                file_name = song.title
+                with open(os.path.join(path, f"{file_name}.txt"), "w") as f:
+                    lyrics = song.lyrics
+                    f.write(lyrics)
+        except:
+            pass
+
+
+add_lyrics(artists, 4)
